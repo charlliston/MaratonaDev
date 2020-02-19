cf9e70ec5c207127ba02d9ace774535a2690322a
System Administrator <root@MacBook-Pro-de-Charlliston.local>
2020-02-18 21:44:06 -0300

    v episodio02

---
+++ js/server.js
@@ -0,0 +1,71 @@
+//configura server
+const express = require("express")
+const server = express()
+
+//apresentaçao arquivos estáticos
+server.use(express.static('public'))
+
+//ativar body do form
+server.use(express.urlencoded({
+    extended: true
+}))
+
+//configurar banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'ch19',
+    host: 'localhost',
+    port: 5432,
+    database: 'doadores'
+})
+
+//template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
+    //noCache: true,
+})
+
+
+//apresentacao da página
+server.get("/", function(req, res) {
+    
+    db.query("SELECT * FROM donors", function(err, result) {
+        if (err) return res.send("Erro de banco de dados.")
+
+        const donors = result.rows
+        return res.render("index.html", {donors})
+    })
+})
+
+server.post("/", function(req, res) {
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios.")
+    }
+
+    //add valores ao bd
+    const query = `
+    INSERT INTO donors ("name", "email", "blood")
+    VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err) {
+        //fluxo de erro
+        if (err) return res.send("Erro de banco de dados.")
+
+        //fluxo ideal
+        return res.redirect("/")
+    })
+})
+
+//ligar servidor e setar porta
+server.listen(3000), function() {
+
+}
